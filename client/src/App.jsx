37884ee878ab4745a7664167bc42936a9d737dd8
Nethmi-24 <batawalann.20@uom.lk>
2024-02-13 20:55:11 +0530

    Complete Header Component is added with styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import SignUp from "./pages/Signup";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
 export default function App() {
   return (
     <div>
       <BrowserRouter>
+      <Header/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/sign-in" element={<SignIn />} />
