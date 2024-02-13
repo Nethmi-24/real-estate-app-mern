e375e824bce25d1e878b9d8706ea673dba2facde
Nethmi-24 <batawalann.20@uom.lk>
2024-02-13 20:16:38 +0530

    Pages and routing added from front

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,22 @@
-import React from 'react'
-import {BrowserRouter,Routes} from 'react-router-dom'
+import React from "react";
+import Home from "./pages/Home";
+import SignIn from "./pages/Signin";
+import SignUp from "./pages/Signup";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 export default function App() {
   return (
-  
-<div>
-  <BrowserRouter>
-  <Routes>
-
-  </Routes>
-  </BrowserRouter>
-</div>
-    
-  )
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
