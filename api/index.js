adcc3b115810cb4e5af5ba0fca213a398c2f823b
Nethmi-24 <batawalann.20@uom.lk>
2024-02-18 19:33:13 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,11 +1,18 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const app=express(); //create an app
-
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000");
+dotenv.config();
+mongoose.connect(process.env.MONGO).then(() => {
+  console.log("Connected to Mongo!");
+}).catch((err)=>{
+    console.log(err);
 })
 
+const app = express(); //create an app
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+
 //install nodemon for refreshing the server. run npm i nodemon on terminal
-
-
